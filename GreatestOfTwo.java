22d51eb60ca60252c2f9ba2a13157a828ce6421c
Bhuvana1822 <bhuvanareddy2211@gmail.com>
2026-02-14 10:43:34 +0530

    Add files via upload

---
+++ GreatestOfTwo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class GreatestOfTwo{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a>b){
+        System.out.println("a is big:");
+    }
+    else{
+        System.out.println("b is big");
+    }
+   }
+}
